bc4609b04f43fecec2c9d7342be2eebb817d607b
GoodGame <yousefmnier@gmail.com>
2024-03-05 09:08:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Name: ibrahim hrbawe
+grade: 11th
+GameReviews is a website made by gamers for gamers to review the games that they played and add a list of what they want to play or played before to track their games over different platforms.
---
+++ src/main/java/com/example/firstWebApp/entities/users.java
@@ -0,0 +1,50 @@
+package com.example.firstWebApp.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "users")
+public class users implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String  email;
+
+    public users() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public users(Long id, String firstName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.email = email;
+
+    }
+}
